48c73703ba45e648e5d70a0d979ff93d3d3c0177
Dario Quinteros <dario.quinteros.bustos@gmail.com>
2023-07-13 20:25:01 -0300

    Practica integradora

--- src/routes/messages.router.js
+++ src/routes/messages.router.js
@@ -1,19 +1,22 @@
 //Librerías 
 import express from "express"
+import Messages from "../dao/dbManagers/messages.js";
 
 // Variables 
 const router = express.Router();
+const messManager = new Messages;
 
 // ChatBox
 router.get ('/', async (req, res)=> {
     res.render('chat', {
         style: 'index.css'
     })
-    /*
-    let messages = messManager.getAll();
+})
+
+router.get ('/messages', async (req, res)=> {
+    let messages = await messManager.getAll();
     if(!messages) return res.status(500).status({status:"error", error:"No se pudo obtener los datos "});
     res.send({status:"success", playload: messages});
-    */
 })
 
 export default router;
